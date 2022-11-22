22f625eb0636d64162022904ad7d5eeab90da51a
Aziz Soorti <hies.aziz2019@gmail.com>
2022-11-22 07:28:18 -0800

    img added

--- src/components/Singin.jsx
+++ src/components/Singin.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
+
 function Singin() {
 
     const [email, setEmail] = useState("")
@@ -8,7 +9,8 @@ function Singin() {
     const [callApi, setCallApi] = useState(false);
     const [checkEmail, setCheckEmail] = useState("")
     const [success, setSuccess] = useState(false)
-
+    const [form, setForm] = useState({ display: "block" });
+    const [img, setImg] = useState({ display: "none" })
     function login(e) {
         e.preventDefault();
         setCallApi(true)
@@ -16,10 +18,12 @@ function Singin() {
             if (checkEmail[i] === email) {
                 setSuccess(true)
                 console.log("successfly matched");
-            } 
+            }
         };
-      
-
+if(success === true){
+    setForm({display:"none"}) ;
+    setImg({display:"block"})
+}
     };
 
 
@@ -45,45 +49,58 @@ function Singin() {
 
     }, [callApi])
 
+
+
+
+
+
+
+
+
     return (
 
-        <> 
-                <main className="form-signin w-100 m-auto " >
-                    <form className='my-5 py-5' onSubmit={(e)=>{login(e)}}>
-        
-                        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-                        <div className="form-floating">
-                            <input onChange={(e) => { setEmail(e.target.value) }}
-                                value={email}
-                                type="email"
-                                className="form-control"
-                                id="floatingInput"
-                                placeholder="name@example.com"
-                            />
-                            <label htmlFor="floatingInput">Email address</label>
-                        </div>
-                        <div className="form-floating">
-                            <input onChange={(e) => { setPassword(e.target.value) }}
-                                value={password}
-                                type="password"
-                                className="form-control"
-                                id="floatingPassword"
-                                placeholder="Password"
-                            />
-                            <label htmlFor="floatingPassword">Password</label>
-                        </div>
-                        <button  className="w-100 btn btn-lg btn-primary" type="submit">
-                            Sign in
-                        </button>
-        
-                    </form>
-                
-                </main>
-        
-        
-                </>
-        
-            )
-        }
+        <>
+            
+            <div className="successful text-center my-5 py-5" style={img }>
+<img src="images/c34428b1-8e49-4b32-882d-ea2f3d95e9c0.jpg" alt="" />
+<h3 className='text-center'>singin has been successfly</h3>
+            </div>
+            <main className="form-signin w-100 m-auto " style={form} >
+                <form className='my-5 py-5'  onSubmit={(e) => { login(e) }}>
+
+                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                    <div className="form-floating">
+                        <input onChange={(e) => { setEmail(e.target.value) }}
+                            value={email}
+                            type="email"
+                            className="form-control"
+                            id="floatingInput"
+                            placeholder="name@example.com"
+                        />
+                        <label htmlFor="floatingInput">Email address</label>
+                    </div>
+                    <div className="form-floating">
+                        <input onChange={(e) => { setPassword(e.target.value) }}
+                            value={password}
+                            type="password"
+                            className="form-control"
+                            id="floatingPassword"
+                            placeholder="Password"
+                        />
+                        <label htmlFor="floatingPassword">Password</label>
+                    </div>
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">
+                        Sign in
+                    </button>
+
+                </form>
+
+            </main>
+
+
+        </>
+
+    )
+}
 
 export default Singin
